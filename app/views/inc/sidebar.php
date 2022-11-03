ad3eb8eddefeb77da323b15edfa0bac9d9942da9
Geno <57450665+Kkura02@users.noreply.github.com>
2022-11-03 01:43:38 -0700

    Collapsable Sidebar Menu Addition

--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -2,35 +2,118 @@
     $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 ?>
 
-<aside class="relative bg-indigo-400 h-screen w-64 hidden sm:block shadow-xl">
+<aside class="relative bg-indigo-400 h-screen w-64 hidden sm:block shadow-xl overflow-x-auto">
         <div class="p-6">
             <a href="<?php echo URLROOT;?>/pages/dashboard" class="text-white text-3xl font-semibold hover:text-gray-300"><?php echo SITENAME?></a>
         </div>
-        <nav class="text-white text-base font-semibold pt-3 ">
-            <a href="<?php echo URLROOT;?>/pages/dashboard" class="flex items-center py-4 pl-6 nav-item text-white <?php if (strpos($url, 'dashboard')) {echo 'active-nav-link';} else {echo ' opacity-75';}?>">
-                <i class="fas fa-tachometer-alt mr-3"></i>
-                Dashboard
-            </a>
-            <a href="<?php echo URLROOT;?>/users/show"  class="flex items-center hover:opacity-100 py-4 pl-6 nav-item text-white <?php if (strpos($url, 'create')){ echo 'active-nav-link ';}else{echo 'opacity-75';}?>">
-                <i class="fas fa-user-cog mr-3"></i>
-                Manage Users
-            </a>
-            <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-table mr-3"></i>
-                Tables (soon...)
-            </a>
-            <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-align-left mr-3"></i>
-                Servers (soon...)
-            </a>
-            <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-tablet-alt mr-3"></i>
-                Databases (soon...)
-            </a>
-            <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-                <i class="fas fa-calendar mr-3"></i>
-                Reports (soon...)
-            </a>
+        <nav class="text-white text-base font-semibold pt-3">
+            <ul class="overflow-auto">
+                <li>
+                    <a href="<?php echo URLROOT;?>/pages/dashboard" class="flex items-center py-4 pl-6 nav-item text-white <?php if (strpos($url, 'dashboard')) {echo 'active-nav-link';} else {echo ' opacity-75';}?>">
+                    <i class="fas fa-tachometer-alt mr-3"></i>
+                    Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo URLROOT;?>/users/show"  class="flex items-center hover:opacity-100 py-4 pl-6 nav-item text-white <?php if (strpos($url, 'show')){ echo 'active-nav-link ';}else{echo 'opacity-75';}?>">
+                    <i class="fas fa-user-cog mr-3"></i>
+                    Manage Users
+                    </a>
+                </li>
+                <li>
+                    <button type="button" class="flex items-center py-4 pl-6 nav-item text-white w-full" aria-controls="dropdown-example1" data-collapse-toggle="dropdown-example1"> <!--Change here for control variables-->
+                            <i class="fas fa-user-cog mr-3"></i>
+                            <span class="flex-1 text-left" sidebar-toggle-item>CHOICE 1</span>
+                                <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                </svg>
+                        </button>
+                            <ul id="dropdown-example1" class="hidden py-2 space-y-2">
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Productsasdasgasgawgaassfasgawgas</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Billing</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Invoice</a>
+                                </li>
+                            </ul>
+                </li>
+                <li>
+                    <button type="button" class="flex items-center py-4 pl-6 nav-item text-white w-full" aria-controls="dropdown-example2" data-collapse-toggle="dropdown-example2">
+                            <i class="fas fa-user-cog mr-3"></i>
+                                <span class="flex-1 text-left" sidebar-toggle-item>CHOICE 1</span>
+                            <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                            <ul id="dropdown-example2" class="hidden py-2 space-y-2">
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Products</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Billasgasgawgasging</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Invoice</a>
+                                </li>
+                            </ul>
+                </li>
+                <li>
+                    <button type="button" class="flex items-center py-4 pl-6 nav-item text-white w-full" aria-controls="dropdown-example3" data-collapse-toggle="dropdown-example3">
+                            <i class="fas fa-user-cog mr-3"></i>
+                                    <span class="flex-1 text-left" sidebar-toggle-item>CHOICE 1</span>
+                            <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                            <ul id="dropdown-example3" class="hidden py-2 space-y-2">
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Products</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Billing</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Invoice</a>
+                                </li>
+                            </ul>
+                </li>
+                <li>
+                    <button type="button" class="flex items-center py-4 pl-6 nav-item text-white w-full" aria-controls="dropdown-example4" data-collapse-toggle="dropdown-example4">
+                            <i class="fas fa-user-cog mr-3"></i>
+                                    <span class="flex-1 text-left" sidebar-toggle-item>CHOICE 1</span>
+                            <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                            <ul id="dropdown-example4" class="hidden py-2 space-y-2">
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Products</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Billing</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 pl-11">Invoice</a>
+                                </li>
+                            </ul>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+                    <i class="fas fa-table mr-3"></i>
+                    Tables (soon...)
+                    </a>
+                </li>
+            </ul>
+            <script src="https://unpkg.com/flowbite@1.3.4/dist/flowbite.js"></script>
         </nav>
     </aside>
     <div class="w-full flex flex-col h-screen overflow-y-hidden">
