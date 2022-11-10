929832c12193f3c2173e6d5db410aeda79da2858
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-11-10 11:26:23 +0800

    Removed pages view & config update

--- app/config/config.php
+++ app/config/config.php
@@ -12,11 +12,11 @@
 
     //ftp df file sites
     define('RMSPRD', 'ftp://oracle:0racleas1@192.168.32.184/home/oracle/dba_scripts/df.txt');
-    define('RDWPRD', 'ftp://oracle:0racleas1@192.168.32.198/home/oracle/dba_scripts/df.txt');
+    define('RDWPRD', 'ftp://oracle:0racleas1@192.168.32.198/u01/oracle/dba_scripts/df.txt');
     define('RMSUAT', 'ftp://oracle:0racleas1@192.168.32.101/home/oracle/dba_scripts/df.txt');
-    define('OFIN', 'ftp://oracle:0racleas1@192.168.34.8/home/oracle/dba_scripts/df.txt');
-    define('ERPP', 'ftp://oracle:0racleas1@192.168.32.44/home/oracle/dba_scripts/df.txt');
-    define('WMSPRD1', 'ftp://oracle:0racleas1@192.168.33.57/home/oracle/dba_scripts/df.txt');
-    define('WMSPRD2', 'ftp://oracle:0racleas1@192.168.33.60/home/oracle/dba_scripts/df.txt');
-    define('OBIEE1', 'ftp://oracle:0racleas1@192.168.32.114/home/oracle/dba_scripts/df.txt');
-    define('OBIEE2', 'ftp://oracle:0racleas1@192.168.32.117/home/oracle/dba_scripts/df.txt');
+    define('OFIN', 'ftp://oraerpp:oraerpp@05:34:08@192.168.34.8/home/oracle/dba_scripts/df.txt');
+    define('ERPP', 'ftp://oracle:0racleas1@192.168.32.44/u01/dba/scripts/df.txt');
+    define('WMSPRD1', 'ftp://oracle:0racleas1@192.168.33.57/u01/dbascripts/df.txt');
+    define('WMSPRD2', 'ftp://oracle:0racleas1@192.168.33.60/u01/dbascripts/df.txt');
+    define('OBIEE1', 'ftp://oracle:0racleas1@192.168.32.114/home/oracle/dba/df.txt');
+    define('OBIEE2', 'ftp://oracle:0racleas1@192.168.32.117/home/oracle/dba/df.txt');
