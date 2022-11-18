7a0c364a7f9327f835f5cdbba3c922725723cab3
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-11-18 13:25:50 +0800

    Update get_TotalSessions.sql

--- app/sql/get_TotalSessions.sql
+++ app/sql/get_TotalSessions.sql
@@ -3,3 +3,4 @@ SELECT COUNT(*) AS "Total Sessions"
         v$session a,v$process b 
             WHERE      a.paddr = b.addr
                 AND        a.username not in ('SYSMAN','TAFRHOSP','DBSNMP','APEX_PUBLIC_USER','ALLOC13PRD','SYS','SYSTEM')
+                AND     a.status = 'ACTIVE'
