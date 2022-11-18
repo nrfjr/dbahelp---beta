a7cc626e62ed5144719a8e4e95def3d1f0f512ba
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-11-18 11:47:52 +0800

    Database info summary sql added

---
+++ app/sql/get_DBInfoSummary.sql
@@ -0,0 +1,26 @@
+ -- DATABASE INFO SUMMARY
+SELECT 
+
+CONCAT((select host_name from v$instance), '.kccmalls.com') AS "Hostname",
+
+UTL_INADDR.get_host_address AS "IP Address",
+
+(select (CASE
+            WHEN SUM(BYTES)/POWER(2,10) < 1024 THEN
+            TO_CHAR(SUM(BYTES)/POWER(2,10),'fm999999') || ' KB'
+            when
+            SUM(BYTES)/POWER(2,20) < 1024 THEN
+            TO_CHAR(SUM(BYTES)/POWER(2,20),'fm999999') || ' MB'
+            when SUM(BYTES)/POWER(2,30) < 1024 THEN
+            TO_CHAR(SUM(BYTES)/POWER(2,30),'fm999999') || ' GB'
+            else
+            TO_CHAR(SUM(BYTES)/POWER(2,40),'fm999999') || ' TB' 
+            END) from v$datafile) AS "DB SIZE",
+
+(select count(*) from v$session) AS "TOTAL",
+
+(select count(*) from v$session where status = 'INACTIVE') AS "INACTIVE",
+
+(select count(*) ACTIVE from v$session where status = 'ACTIVE' and username is not null) AS "ACTIVE",
+
+(select count(*) SYSTEM from v$session where username is null) AS "SYSTEM" FROM DUAL
