3512f12b00784af589edc662f5ba05441ea13b0b
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-11-23 05:29:38 +0800

    Update footer.php

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,5 @@
 </main>
-    <footer class="w-full bg-gray-700 p-4 bottom-0 text-white">
+    <footer class="w-full bg-gray-700 p-4 flex justify-end text-white">
         DBAHelp Version 1.0.0.001
     </footer>
 </body>
