a127307798ee0ab55cebbd8b6344ace0a32dbd00
Geno <57450665+Kkura02@users.noreply.github.com>
2022-11-23 22:33:55 -0800

    final fix on collapsible sidebar responsiveness

--- public/js/sideNav.js
+++ public/js/sideNav.js
@@ -1,10 +1,22 @@
 function navToggle() {
     var x = document.getElementById("myNav");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    x.style.position = "fixed";
-  } else {
-    x.style.display = "none";
-    x.style.position = "relative";
-  }
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      if(window.matchMedia("(min-width: 768px)")){
+        x.style.position = "relative;"
+      }else{
+        x.style.position = "fixed";
+      }
+    } else{
+      x.style.display = "none";
+      // if(window.matchMedia("(min-width: 768px)")){
+      //   x.style.position = "relative;"
+      // }else{
+      //   x.style.position = "fixed";
+      // }
+    }
+  
+
+  
   }
