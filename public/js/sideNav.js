f72a1aeb50b608652c7fd50cd234285783a8c004
Geno <57450665+Kkura02@users.noreply.github.com>
2022-11-23 21:54:28 -0800

    improved the sidenavbar toggle function
    position property is also changed when button is clicked. relative-fixed

--- public/js/sideNav.js
+++ public/js/sideNav.js
@@ -2,7 +2,9 @@ function navToggle() {
     var x = document.getElementById("myNav");
   if (x.style.display === "none") {
     x.style.display = "block";
+    x.style.position = "fixed";
   } else {
     x.style.display = "none";
+    x.style.position = "relative";
   }
   }
