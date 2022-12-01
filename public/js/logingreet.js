b073a9f57a1c1bf6bb0ab98f7313494731d41a15
Geno <57450665+Kkura02@users.noreply.github.com>
2022-11-30 21:45:39 -0800

    Add files via upload

---
+++ public/js/logingreet.js
@@ -0,0 +1,18 @@
+var i = 0;
+var txt = 'GOOD DAY DBA!'; /* The text */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("greet").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+setTimeout(function typeWriter(){
+  if (i < txt.length) {
+    document.getElementById("greet").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}, 2000);
