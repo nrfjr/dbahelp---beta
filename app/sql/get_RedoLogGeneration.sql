d97285086bdce8abab2382e4aa264ca97622f39a
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-12-05 11:25:29 +0800

    Query for redo log generation

---
+++ app/sql/get_RedoLogGeneration.sql
@@ -0,0 +1,11 @@
+SELECT    
+        TRUNC(COMPLETION_TIME) AS "Run Date",
+        COUNT(*)  "Switch Count",
+        ROUND((SUM(BLOCKS*BLOCK_SIZE)/1024/1024)) "Redo Log/s Per Day"
+	FROM      
+        V$ARCHIVED_LOG
+	WHERE     
+        TRUNC(COMPLETION_TIME) > SYSDATE-11
+	GROUP BY  
+        TRUNC(COMPLETION_TIME)
+	ORDER BY  1 ASC
