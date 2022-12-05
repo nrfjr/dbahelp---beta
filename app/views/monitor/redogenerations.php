8f0164bbb1cdece066472b66a332bbade7480d18
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-12-05 12:45:15 +0800

    Added chart values.

--- app/views/monitor/redogenerations.php
+++ app/views/monitor/redogenerations.php
@@ -1,20 +1,40 @@
 <?php
 $title = 'Redo Log Generation';
 require APPROOT . '/views/inc/header.php';
-require APPROOT . '/views/inc/sidebar.php'; ?>
+require APPROOT . '/views/inc/sidebar.php';
+
+$filterPHParray = function ($array, $key) {
+   
+    foreach ($array as $title => $arrays) {
+        $Selected[] = $arrays[$key];
+    }
+    
+    $last = end($Selected);
+    foreach($Selected as $log){
+        if($last == $log){
+            echo '\''.$log.'\'';
+        }else{
+            echo '\''.$log.'\', ';
+        }
+
+    }
+};
+
+?>
 
 <div class="flex justify-between mb-2">
     <h1 class="text-3xl text-black text-white">
         <a href="<?php echo URLROOT; ?>/homepages/index/<?php echo $_SESSION['MonitorDB']; ?>" class="no-underline hover:underline">Monitor</a> > <b>Redo Log Generation</b>
     </h1>
-    <button type="submit" class="px-4 py-2 rounded-lg bg-gray-300 hover:bg-gray-500 shadow-inner shadow-xl">
-                Refresh Data
-            </button>
+    <a href="<?php echo URLROOT; ?>/monitors/redologgenerations/<?php echo $_SESSION['MonitorDB'] ?>"><button type="submit" class="px-4 py-2 rounded-lg bg-gray-300 hover:bg-gray-500 shadow-inner shadow-xl">
+            Refresh Data
+        </button></a>
 </div>
 
 <div class="flex flex-col justify-between h-full gap-y-1" style="max-height:93% ;">
-    <div class="flex flex-col gap-y-1">
-        <div id="redochart" class="bg-gray-300 rounded-md h-fit boxx"></div>
+    <div class="flex flex-col">
+        <div id="redochart" class="bg-gray-300 rounded-md h-fit">
+        </div>
     </div>
     <div class="block justify-center lg:h-full w-full shadow-md overflow-auto sm:rounded-lg lg:max-h-80 xl:max-h-96">
         <?php
@@ -75,10 +95,9 @@ require APPROOT . '/views/inc/sidebar.php'; ?>
 
 <script>
     var options = {
-        colors: ['#00ff00', '#1aff1a', '#33ff33', '#4dff4d'],
         series: [{
             name: '',
-            data: [1086, 624, 1540, 1498, 1634, 1138, 1448, 1894, 1288, 1208, 842]
+            data: [<?php $filterPHParray($data, 'Switch Count'); ?>]
         }],
         chart: {
             height: 350,
@@ -91,42 +110,20 @@ require APPROOT . '/views/inc/sidebar.php'; ?>
             enabled: false
         },
         stroke: {
-            curve: 'smooth',
-            
+            curve: 'smooth'
         },
         title: {
             text: '<?php echo $_SESSION['MonitorDB']; ?> Redo Log Generation per Day',
-            align: 'left',
-            style:{
-                color: 'white'
-            }
+            align: 'left'
         },
         grid: {
             row: {
-                colors: ['#8c8c8c', '#2c2f33'], // takes an array which will be repeated on columns
+                colors: ['#8c8c8c', 'transparent'], // takes an array which will be repeated on columns
                 opacity: 0.5
             },
         },
         xaxis: {
-            categories: ['19-NOV-22', '20-NOV-22', '21-NOV-22', '22-NOV-22', '23-NOV-22', '24-NOV-22', '25-NOV-22', '26-NOV-22', '27-NOV-22', '28-NOV-22', '29-NOV-22'],
-            labels:{
-                style:{
-                    colors: 'white'
-                },
-            },
-            axisBorder:{
-                color: 'white'
-            }
-        },
-        yaxis:{
-            axisBorder:{
-                color: 'white'
-            },
-            labels:{
-                style:{
-                    colors: 'white'
-                }
-            }
+            categories: [<?php $filterPHParray($data, 'Date Generated'); ?>],
         }
     };
 
