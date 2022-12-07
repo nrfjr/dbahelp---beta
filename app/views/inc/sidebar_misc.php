c085d2875c5540154a6c6283469d608c579b62b3
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-12-08 07:19:08 +0800

    Update includes view files

---
+++ app/views/inc/sidebar_misc.php
@@ -0,0 +1,11 @@
+<div class="left-0 widgets transition ease-in-out delay-150 bg-gradient-to-br from-gray-500 to-gray-900 rounded-lg">
+        <div class="w-9/10 justify-center px-1 mt-1">
+            <div id="swtClock" class="bg-gradient-to-r from-pink-100 to-blue-100 rounded-t-lg" style="text-align: center;"></div>
+            <script type="text/javascript" src="https://www.superwebtricks.com/wp-content/uploads/clock.js.txt"></script>
+
+        </div>
+        <!--Calendar-->
+        <div class="w-full px-1">
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23039BE5&ctz=Asia%2FManila&showTitle=0&showTz=0&showCalendars=0&showPrint=0&showTabs=0&src=YmI1ZDVkMjgxZThjNTQ5MWYyZGRkOTBlMGQ5YzZkYWU4NWI2ODcxNzc5OGI5ZjE0NTI5ZDFiZGQzZWZmNGUxMUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23795548" style="border-width:0; border-radius: 0 0 5px 5px;pointer-events:none;" class="shadow-lg" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+        </div>
+</div>
