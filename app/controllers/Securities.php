642a961d5e332e97976b7e5dee456b07a1c5433d
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-12-12 16:57:55 +0800

    Added text file editor.

--- app/controllers/Securities.php
+++ app/controllers/Securities.php
@@ -11,14 +11,55 @@ class Securities extends Controller{
         }
     }
 
-    public function roleprivilege()
+    public function roleprivilege($DB)
     {
+        $_SESSION['SecurityDB'] = $DB;
+
+        isset($_SESSION['SecurityDB']);
+
         $this->view('security/roleprivilege',[]);
     }
 
-    public function ldifforsso()
+    public function ldifforsso($DB)
     {
+        $_SESSION['SecurityDB'] = $DB;
+
+        isset($_SESSION['SecurityDB']);
+
         $this->view('security/ldifforsso',[]);
+    }
+
+    public function download($DB)
+    {
+        $_SESSION['SecurityDB'] = $DB;
+
+        isset($_SESSION['SecurityDB']);
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $raw_contents = trim($_POST['contents']);
+        }
+
+        $filtered_contents = explode('\n', $raw_contents);
+        $file = fopen("MyLDIF.ldif", "w") or die("Unable to open file!");
+        foreach($filtered_contents as $contents){
+            fwrite($file, $contents.PHP_EOL);
+        }
+        fclose($file);
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename('MyLDIF.ldif'));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize('MyLDIF.ldif'));
+        ob_clean();
+        flush();
+        readfile('MyLDIF.ldif');
+        exit();
+        $this->view('securities/ldifforsso/'.$DB, []);
+        
     }
     
 }
--- app/views/security/ldifforsso.php
+++ app/views/security/ldifforsso.php
@@ -3,86 +3,100 @@ $title = 'Create LDIF for SSO';
 require APPROOT . '/views/inc/header.php';
 require APPROOT . '/views/inc/sidebar.php'; ?>
 
-<h1 class="text-3xl text-black pb-2 text-white">
-    <a href="<?php echo URLROOT; ?>/homepage/dashboard" class="no-underline hover:underline">Security</a> > <b>Create LDIF for SSO</b>
+<h1 class="text-3xl text-black mb-5 text-white">
+    <a href="<?php echo URLROOT; ?>/homepages/index/<?php echo $_SESSION['SecurityDB']; ?>" class="no-underline hover:underline">Security</a> > <b>Create LDIF for SSO</b>
 </h1>
 
 <div class="bg-gray-400 rounded-lg p-2 h-">
-    <div id="editor" class="border-none bg-gray-100 rounded-md focus:ring-2" >
-        <p class="text-sm text-black">dn: cn=JDDOE213213,cn=Users,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">objectclass: top</p>
-        <p class="text-sm text-black">objectclass: organizationalperson</p>
-        <p class="text-sm text-black">objectclass: person</p>
-        <p class="text-sm text-black">objectclass: inetorgperson</p>
-        <p class="text-sm text-black">objectclass: KCCOBJ</p>
-        <p class="text-sm text-black">objectclass: rsimUser</p>
-        <p class="text-sm text-black">givenname: JOHN</p>
-        <p class="text-sm text-black">sn: DOE</p>
-        <p class="text-sm text-black">cn: JDDOE213213</p>
-        <p class="text-sm text-black">uid: JDDOE213213</p>
-        <p class="text-sm text-black">employeenumber: 213213</p>
-        <p class="text-sm text-black">mail: JDDOE213213@kccmalls.com</p>
-        <p class="text-sm text-black">description: Department Store</p>
-        <p class="text-sm text-black">displayname: JDDOE213213</p>
-        <p class="text-sm text-black">preferredlanguage: en</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=2,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=1,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=3,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=4,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=5,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=6,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=7,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=8,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=9,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=41,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=42,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=43,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=45,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=46,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=44,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=47,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=48,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userstore: rsimStoreId=49,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">employmentstatus: 0</p>
-        <p class="text-sm text-black">ssn: 123456789</p>
-        <p class="text-sm text-black">referredcountry: US</p>
-        <p class="text-sm text-black">userrole: rsimRoleName=KCC Admin,cn=rsimRoles,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">homestore: rsimStoreId=2,cn=rsimStores,cn=RSIM,dc=kccmalls,dc=com</p>
-        <p class="text-sm text-black">userpassword: bka87701</p>
-    </div>
-    <div class="flex justify-end gap-4 p-2 pt-4">
-        <button class="py-2 px-4 bg-green-400 hover:bg-green-600 rounded-md">Save</button>
-        <button class="py-2 px-4 bg-red-400 hover:bg-red-600 rounded-md">Cancel</button>
-    </div>
+    <form action="<?php echo URLROOT; ?>/securities/download/<?php echo $_SESSION['SecurityDB']; ?>" method="POST">
+        <div id="editor" class="border-none bg-gray-100 rounded-md focus:ring-2">
+            <?php
+            $count = 0;
+            foreach (new SplFileObject('../public/ldifs/sample.ldif') as $line) {
+            ?>
+                <p><?php echo str_replace('>', '&gt;', str_replace('<', '&lt;', $line));?></p>
+
+            <?php
+                $count++;
+            }
+            ?>
+        </div>
+        <div class="flex justify-end gap-4 p-2 pt-4">
+            <input id="contents" class="hidden" name="contents"/>
+            <button onclick="save()" type="submit" class="py-2 px-4 text-white bg-green-400 hover:bg-green-600 rounded-md">Save</button>
+    </form>
 </div>
+</div>
+
+<script>
+function save(){
+    var contents = "";
+        document.querySelectorAll('p').forEach(p=>{
+            contents +=  p.textContent+'\\n';
+        })
+        document.getElementById("contents").value = contents;
+    }
+</script>
 
 
 <!-- Initialize Quill editor -->
 <script>
     var toolbarOptions = [
-  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-  ['blockquote', 'code-block'],
+        ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+        ['blockquote', 'code-block'],
 
-  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-  [{ 'direction': 'rtl' }],                         // text direction
+        [{
+            'header': 1
+        }, {
+            'header': 2
+        }], // custom button values
+        [{
+            'list': 'ordered'
+        }, {
+            'list': 'bullet'
+        }],
+        [{
+            'script': 'sub'
+        }, {
+            'script': 'super'
+        }], // superscript/subscript
+        [{
+            'indent': '-1'
+        }, {
+            'indent': '+1'
+        }], // outdent/indent
+        [{
+            'direction': 'rtl'
+        }], // text direction
 
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{
+            'size': ['small', false, 'large', 'huge']
+        }], // custom dropdown
+        [{
+            'header': [1, 2, 3, 4, 5, 6, false]
+        }],
 
-  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
-  [{ 'align': [] }],
+        [{
+            'color': []
+        }, {
+            'background': []
+        }], // dropdown with defaults from theme
+        [{
+            'font': []
+        }],
+        [{
+            'align': []
+        }],
 
-  ['clean']                                         // remove formatting button
-];
+        ['clean'] // remove formatting button
+    ];
 
-  var editor = new Quill('#editor', {
-    modules: { toolbar: toolbarOptions },
-    theme: 'snow',
-  });
+    var editor = new Quill('#editor', {
+        modules: {
+            toolbar: toolbarOptions
+        },
+        theme: 'snow',
+    });
 </script>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
