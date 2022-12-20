e01bbf0ecf3b866e150ca525bf64f7563a10782a
nrfjr <90370309+nrfjr@users.noreply.github.com>
2022-12-21 07:37:54 +0800

    Update footer.php

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,6 +1,8 @@
 </main>
-    <footer class="w-full bg-gray-700 p-4 flex justify-end text-white">
-        DBAHelp Version 1.0.0.001
-    </footer>
+<footer class="w-full bg-gray-700 p-4 flex justify-end text-white">
+    <a href="https://github.com/nrfjr/dbahelp" target="_blank"><i class="mt-1 mr-2 fab fa-github" title="Check us out on Github! :>"></i></a>
+    <p title="DBAHelp Project Version">DBAHelp Version 1.0.0.001</p>
+</footer>
 </body>
+
 </html>
