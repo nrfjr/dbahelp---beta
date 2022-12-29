ec390dc883564778462b510aa0a44061cb0f9e46
Geno <57450665+Kkura02@users.noreply.github.com>
2022-12-28 18:52:10 -0800

    RedoLogZoom.js for the redologswitches function zoom

---
+++ public/js/RedologZoomPop.js
@@ -0,0 +1,12 @@
+function showPopAM(){
+    document.getElementById("redologAMZoom").classList.remove("hidden");
+}
+function showPopPM(){
+    document.getElementById("redologPMZoom").classList.remove("hidden");
+}
+function hidePopAM(){
+    document.getElementById("redologAMZoom").classList.add("hidden");
+}
+function hidePopPM(){
+    document.getElementById("redologPMZoom").classList.add("hidden");
+}
