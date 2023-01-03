b60f0fe7528803dd4085eec5dff78eff7f131e86
Geno <57450665+Kkura02@users.noreply.github.com>
2023-01-02 18:05:57 -0800

    Readjusted the user create modal
    fixed to adjust on small screen sizes, and also rework the Html sturcture of the Modal

--- app/views/oracle/users/create.php
+++ app/views/oracle/users/create.php
@@ -3,6 +3,7 @@ $title = ($data == null) ? 'Create User' : 'Edit User';
 require APPROOT . '/views/inc/header.php';
 require APPROOT . '/views/inc/sidebar.php'; ?>
 
+<div class="relative" x-data="{toSubmit: false}">
 <h1 class="text-3xl text-black pb-6 text-white"><b><?php echo $data == null ? 'Create User' : 'Edit User'; ?></b></h1>
 <!-- <label for="database" class="absolute right-0 text-sm font-medium text-gray-700">Database</label>
       <select id="database" name="database" autocomplete="database" class="mr-6 absolute right-0 block w-64 rounded-md border  bg-white py-2 px-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" >
@@ -25,7 +26,7 @@ require APPROOT . '/views/inc/sidebar.php'; ?>
       </div>
     </div>
     <div class="mt-5 xl:col-span-2 md:mt-0 justify-evenly">
-      <div class="bg-white overflow-hidden shadow sm:rounded-md pb-44 relative">
+      <div class="bg-white overflow-hidden shadow rounded-md pb-44 relative">
         <form name="Create User" action="<?php echo URLROOT; ?>/users/create/<?php echo $_SESSION['CreateUserDB']; ?>" method="POST">
           <div class="bg-white px-4 py-5 sm:p-6">
             <div class="grid mt-10 grid-cols-6 gap-16">
@@ -83,38 +84,14 @@ require APPROOT . '/views/inc/sidebar.php'; ?>
             </div>
           </div>
           <div class="bg-gray-50 px-4 py-3 text-right sm:px-6 absolute inset-x-0 bottom-0">
-            <div x-data="{toSubmit: false}">
+            <div >
               <button type="button" @click="toSubmit = true" class="inline-flex justify-center rounded-md border border-transparent bg-green-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
               <?php echo $data == null ? 'Create' : 'Edit'; ?>
                 <i class="fas mt-1 fa-user-plus ml-2"></i>
               </button>
               <button x-show="toSubmit" @click="toSubmit = false" class="border-blue-500 md:border-green-500">
               </button>
-              <!-- Delete User Modal -->
-              <div x-show="toSubmit" class="border-double border-2 border-red-500 absolute -left-14 m-auto z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-                <div class="modal fixed fade justify-center mr-48 top-72 w-5/12 h-full outline-none overflow-x-hidden overflow-y-auto" id="ModalCenteredScrollable" tabindex="-1" aria-labelledby="ModalCenteredScrollable" aria-modal="true" role="dialog">
-                  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable relative pointer-events-none w-auto">
-
-                    <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-                      <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-                        <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalCenteredScrollableLabel">
-                          <b>Confirm <?php echo $data == null ? 'User Creation' : 'User Modification'; ?></b>
-                        </h5>
-                        <button type="button" @click="toSubmit = false" class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body text-center p-4">
-                        <font color="black"><b>Are you sure to <?php echo $data == null ? 'create' : 'edit'; ?> this user ?</b></font>
-                      </div>
-                      <div class="modal-footer flex justify-end p-4 border-t border-gray-200 rounded-b-md">
-                        <button type="submit" class="rounded-md border border-transparent bg-green-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"><?php echo $data == null ? 'Create' : 'Edit'; ?></button>
-                        <button type="button" @click="toSubmit = false" class="rounded-md border  bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- Delete User Modal -->
+              
             </div>
           </div>
         </form>
@@ -129,6 +106,32 @@ require APPROOT . '/views/inc/sidebar.php'; ?>
       </div>
     </div>
   </div>
+</div>
+<!-- Create User Modal -->
+<div x-show="toSubmit" class="absolute w-full h-full left-0 top-0 z-10 " aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                  <div class="flex fixed w-full h-full inset-0 bg-gray-500 bg-opacity-75 transition-opacity border-4"></div>
+                  <div class="modal fade absolute flex justify-center items-center xl:px-36 w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="ModalCenteredScrollable" tabindex="-1" aria-labelledby="ModalCenteredScrollable" aria-modal="true" role="dialog">
+                    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable static pointer-events-none w-full xl:w-1/2">
+
+                      <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                        <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+                          <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalCenteredScrollableLabel">
+                            <b>Confirm <?php echo $data == null ? 'User Creation' : 'User Modification'; ?></b>
+                          </h5>
+                          <button type="button" @click="toSubmit = false" class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body text-center p-4">
+                          <font color="black"><b>Are you sure to <?php echo $data == null ? 'create' : 'edit'; ?> this user ?</b></font>
+                        </div>
+                        <div class="modal-footer flex justify-end p-4 border-t border-gray-200 rounded-b-md">
+                          <button type="submit" class="rounded-md border border-transparent bg-green-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"><?php echo $data == null ? 'Create' : 'Edit'; ?></button>
+                          <button type="button" @click="toSubmit = false" class="rounded-md border  bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              <!-- Create User Modal -->
 </div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
