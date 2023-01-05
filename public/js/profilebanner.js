4ec254cb528e1a2eaf2673d5b4fa3ece50a48209
nrfjr <90370309+nrfjr@users.noreply.github.com>
2023-01-05 14:14:34 +0800

    Update profilebanner.js

--- public/js/profilebanner.js
+++ public/js/profilebanner.js
@@ -1,25 +1,11 @@
-function bannerColor(color1,color2){
-    document.getElementById("banner").classList.remove("hidden")
-    document.getElementById("banner").classList.remove("bg-gradient-to-r")
-    document.getElementById("banner").classList.remove("from-gray-400")
-    document.getElementById("banner").classList.remove("to-gray-900")
-    document.getElementById("banner").style.background = "linear-gradient(90deg, "+color1.toLocaleString()+", "+color2.toLocaleString()+")";
-}
-
-//Banner Use
-let bcolor1; //global variable for color gradient 1
-let bcolor2; //global variable for color gradient 1
-
 //Banner Use
 //function for the color option button in the addTodomodal
-function getColor(color1,color2){
-    bcolor1 = color1;
-    bcolor2 = color2;
-    document.getElementById("colorIcon").style.background = "linear-gradient(90deg, "+color1.toLocaleString()+", "+color2.toLocaleString()+")";
+function getColor(deg,color1,color2){
+    document.getElementById("colorIcon").style.background = "linear-gradient("+deg.toLocaleString()+", "+color1.toLocaleString()+", "+color2.toLocaleString()+")";
     // console.log(todoColor1,todoColor2)
-    
-    saveColor(bcolor1,bcolor2);
-    
+
+    saveColor(deg.toLocaleString(),color1.toLocaleString(),color2.toLocaleString());
+
 }
 
 const getColors = () => {
@@ -32,11 +18,12 @@ const getColors = () => {
     return colors;
 }
 
-const saveColor = (Color1,Color2) => {
+const saveColor = (Deg,Color1,Color2) => {
     const colors = getColors();
-    colors.push(Color1,Color2);
-    localStorage.setItem(0, JSON.stringify(colors));
     console.log(colors);
+    colors.push(Deg,Color1,Color2);
+    localStorage.setItem(0, JSON.stringify(colors));
+    // console.log(colors);
     applyColor();
 }
 
@@ -46,6 +33,6 @@ const applyColor = () => {
     if(list.length > 0){
         document.querySelector("#defaultimage").classList.add("hidden");
         document.querySelector("#banner").classList.remove("hidden","bg-gradient-to-r","from-gray-400","to-gray-900");
-        document.querySelector("#banner").style.background = "linear-gradient(90deg, "+list[0].toLocaleString()+", "+list[1].toLocaleString()+")";
+        document.querySelector("#banner").style.background = "linear-gradient("+list[0].toLocaleString()+", "+list[1].toLocaleString()+", "+list[2].toLocaleString()+")";
     }
 }
