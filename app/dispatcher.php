bd12ae16a343e834466a4f4adc8daf3dad3c89fb
nrfjr <90370309+nrfjr@users.noreply.github.com>
2023-01-06 13:27:50 +0800

    updated autoload classes method

--- app/dispatcher.php
+++ app/dispatcher.php
@@ -1,27 +1,24 @@
 <?php
-    /***
-     *  I can't figure out why these files
-     *  cannot be included in spl_autoload_register.
-     *  For now, i'll be storing them in this un-spl_autoload_register
-     *  array, in this way it is easy to include new files to be
-     *  required_once. i'll be looking further for possible solution for this.
-    ***/
-    
-    $UNSPLs = [
-                'config/config.php',
-                'helpers/url_helper.php',
-                'helpers/session_helper.php',
-                'helpers/tab_helper.php',
-                'filters/sanitize.php',
-                'misc/Greeting.php',
-                'interfaces/DBInterface.php',
-            ];
 
-    foreach($UNSPLs as $UNSPL){
-            require_once $UNSPL;
-    }
-
-    // Autoload Core Libraries
+    // Autoload All Necessary Files and Libraries
     spl_autoload_register(function($className) {
-        require_once 'libs/' .$className. '.php';
+
+        $fileMap = [
+            'Greeting' => 'misc/Greeting.php',
+            'DBInterface' => 'interfaces/DBInterface.php',
+            'config' => 'config/config.php',
+            'url_helper' => 'helpers/url_helper.php',
+            'session_helper' => 'helpers/session_helper.php',
+            'tab_helper' => 'helpers/tab_helper.php',
+            'sanitize' => 'filters/sanitize.php'
+        ];
+    
+        if ($fileMap){
+            foreach($fileMap as $k => $v){
+                require_once $v;
+            }
+        }
+
+        require_once 'libs/' . $className . '.php';
+
     });
