181a3b89b3e618fb6aa32e8ecc8d050a105f85dc
nrfjr <90370309+nrfjr@users.noreply.github.com>
2023-01-17 13:49:55 +0800

    Fixed html entities conversion on edit

--- public/js/dbserverlist.js
+++ public/js/dbserverlist.js
@@ -1,6 +1,6 @@
-function hidemodal(){
+function hidemodal() {
     document.querySelector("#createmodal").classList.add("hidden");
-    
+
     //resets the inputs value in dbform upon clicking cancel
     document.querySelector("#iddb").value = "";
     document.querySelector("#hostnamedb").value = "";
@@ -18,10 +18,10 @@ function hidemodal(){
     document.querySelector("#osunameapp").value = "";
     document.querySelector("#ospassapp").value = "";
     document.querySelector("#affilapp").value = "";
-    document.querySelector("#appurlapp").value = ""; 
+    document.querySelector("#appurlapp").value = "";
 }
 
-function createDBmodal(){
+function createDBmodal() {
     document.querySelector("#createmodal").classList.remove("hidden");//shows modal
     document.querySelector("#APPform").classList.add("hidden");//hides appform 
     document.querySelector("#DBform").classList.remove("hidden");//shows dbform 
@@ -29,7 +29,7 @@ function createDBmodal(){
     document.querySelector("#conbtndb").innerHTML = "Create";//replace confirm btn text
 }
 
-function createAPPmodal(){
+function createAPPmodal() {
     document.querySelector("#createmodal").classList.remove("hidden");//shows modal
     document.querySelector("#DBform").classList.add("hidden");//hides dbform
     document.querySelector("#APPform").classList.remove("hidden");//shows appform
@@ -38,24 +38,24 @@ function createAPPmodal(){
     document.querySelector("#appurlapp").value = "http://"
 }
 
-function editmodal(id,type){
-    if(type.trim() == "proddb"  ){
-        editDBmodal(id,"PRD");
-    }else if(type.trim() ==  "prodapps"){
-        editAPPSmodal(id,"PRD");
+function editmodal(id, type) {
+    if (type.trim() == "proddb") {
+        editDBmodal(id, "PRD");
+    } else if (type.trim() == "prodapps") {
+        editAPPSmodal(id, "PRD");
     }
-    else if(type.trim() == "devdb"){
-        editDBmodal(id,"DEV");
+    else if (type.trim() == "devdb") {
+        editDBmodal(id, "DEV");
     }
-    else if(type.trim() == "devapps"){
-        editAPPSmodal(id,"DEV");
+    else if (type.trim() == "devapps") {
+        editAPPSmodal(id, "DEV");
     }
 }
 
-function editDBmodal(id,dbtyped){
-    if (dbtyped == "PRD"){
+function editDBmodal(id, dbtyped) {
+    if (dbtyped == "PRD") {
         document.querySelector("#dbtype").value = "PRD";//sets the value of <select> if dbtyped is PRD
-    }else if (dbtyped == "DEV"){
+    } else if (dbtyped == "DEV") {
         document.querySelector("#dbtype").value = "DEV";//sets the value of <select> if dbtyped is DEV
     }
 
@@ -66,29 +66,29 @@ function editDBmodal(id,dbtyped){
     document.querySelector("#conbtndb").innerHTML = "Edit";//replace confirm btn text
 
     //get value from table first
-    let hostName = document.querySelector("#Hostname"+id).innerHTML;
-    let ipAdd = document.querySelector("#IP_Address"+id).innerHTML;
-    let dbVersion = document.querySelector("#DB_Version"+id).innerHTML;
-    let oS = document.querySelector("#OS"+id).innerHTML;
-    let osUname = document.querySelector("#OS_Username"+id).innerHTML;
-    let osPass = document.querySelector("#OS_Password"+id).innerHTML;
-    let aFfil = document.querySelector("#Affiliation"+id).innerHTML;
+    let hostName = document.querySelector("#Hostname" + id).innerHTML;
+    let ipAdd = document.querySelector("#IP_Address" + id).innerHTML;
+    let dbVersion = document.querySelector("#DB_Version" + id).innerHTML;
+    let oS = document.querySelector("#OS" + id).innerHTML;
+    let osUname = document.querySelector("#OS_Username" + id).innerHTML;
+    let osPass = document.querySelector("#OS_Password" + id).innerHTML;
+    let aFfil = document.querySelector("#Affiliation" + id).innerHTML;
 
     //set values to form
     document.querySelector("#iddb").value = id;
-    document.querySelector("#hostnamedb").value = hostName.trim();
-    document.querySelector("#ipaddressdb").value = ipAdd.trim();
-    document.querySelector("#dbversiondb").value = dbVersion.trim();
-    document.querySelector("#osdb").value = oS.trim();
-    document.querySelector("#osunamedb").value = osUname.trim();
-    document.querySelector("#ospassdb").value = osPass.trim();
-    document.querySelector("#affildb").value = aFfil.trim();
+    document.querySelector("#hostnamedb").value = replaceHTMLEntities(hostName.trim());
+    document.querySelector("#ipaddressdb").value = replaceHTMLEntities(ipAdd.trim());
+    document.querySelector("#dbversiondb").value = replaceHTMLEntities(dbVersion.trim());
+    document.querySelector("#osdb").value = replaceHTMLEntities(oS.trim());
+    document.querySelector("#osunamedb").value = replaceHTMLEntities(osUname.trim());
+    document.querySelector("#ospassdb").value = replaceHTMLEntities(osPass.trim());
+    document.querySelector("#affildb").value = replaceHTMLEntities(aFfil.trim());
 }
 
-function editAPPSmodal(id,apptyped){
-    if (apptyped == "PRD"){
+function editAPPSmodal(id, apptyped) {
+    if (apptyped == "PRD") {
         document.querySelector("#apptype").value = "PRD";//sets the value of <select> if apptyped is PRD
-    }else if (apptyped == "DEV"){
+    } else if (apptyped == "DEV") {
         document.querySelector("#apptype").value = "DEV";//sets the value of <select> if apptyped is DEV
     }
 
@@ -98,22 +98,35 @@ function editAPPSmodal(id,apptyped){
     document.querySelector("#appformtitle").innerHTML = "Edit Application";//replace title text
     document.querySelector("#conbtnapp").innerHTML = "Edit";//replace confirm btn text
 
-    //get value from table first
-    let hostName = document.querySelector("#Hostname"+id).innerHTML;
-    let ipAdd = document.querySelector("#IP_Address"+id).innerHTML;
-    let oS = document.querySelector("#OS"+id).innerHTML;
-    let osUname = document.querySelector("#OS_Username"+id).innerHTML;
-    let osPass = document.querySelector("#OS_Password"+id).innerHTML;
-    let aFfil = document.querySelector("#Affiliation"+id).innerHTML;
-    let appURL = document.querySelector("#Application_URL"+id).textContent;
+    // get value from table first
+    let hostName = document.querySelector("#Hostname" + id).innerHTML;
+    let ipAdd = document.querySelector("#IP_Address" + id).innerHTML;
+    let oS = document.querySelector("#OS" + id).innerHTML;
+    let osUname = document.querySelector("#OS_Username" + id).innerHTML;
+    let osPass = document.querySelector("#OS_Password" + id).innerHTML;
+    let aFfil = document.querySelector("#Affiliation" + id).innerHTML;
+    let appURL = document.querySelector("#Application_URL" + id).textContent;
 
-    //set values to form
+    // set values to form
     document.querySelector("#idapp").value = id;
-    document.querySelector("#hostnameapp").value = hostName.trim();
-    document.querySelector("#ipaddressapp").value = ipAdd.trim();
-    document.querySelector("#osapp").value = oS.trim();
-    document.querySelector("#osunameapp").value = osUname.trim();
-    document.querySelector("#ospassapp").value = osPass.trim();
-    document.querySelector("#affilapp").value = aFfil.trim();
-    document.querySelector("#appurlapp").value = appURL.trim();
+    document.querySelector("#hostnameapp").value = replaceHTMLEntities(hostName.trim());
+    document.querySelector("#ipaddressapp").value = replaceHTMLEntities(ipAdd.trim());
+    document.querySelector("#osapp").value = replaceHTMLEntities(oS.trim());
+    document.querySelector("#osunameapp").value = replaceHTMLEntities(osUname.trim());
+    document.querySelector("#ospassapp").value = replaceHTMLEntities(osPass.trim());
+    document.querySelector("#affilapp").value = replaceHTMLEntities(aFfil.trim());
+    document.querySelector("#appurlapp").value = replaceHTMLEntities(appURL.trim());
+}
+
+function replaceHTMLEntities(value) {
+
+    var specialChars = [{key:'&nbsp;',value: ' ' }, {key:'&lt;',value: '<' }, {key:'&gt;',value: '>' }, {key:'&amp;',value: '&' }, {key:'&quot;',value: '"' }, {key:'&apos;',value: '\'' }, {key:'&cent;',value: '¢' }, {key:'&pound;',value: '£' }, {key:'&yen;',value: '¥' }, {key:'&euro;',value: '€' }, {key:'&copy;',value: '©' }, {key:'&reg;',value: '®' }];
+    
+    var resultKey = specialChars.find(char => value.includes(char.key));
+    
+    if(resultKey){
+        return value.replace(resultKey.key, resultKey.value);
+    }
+    return value;
+    
 }
