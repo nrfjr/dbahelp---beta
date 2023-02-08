8bc748917e451b0a4eaf4dc77b5aa74ec0029f0a
nrfjr <90370309+nrfjr@users.noreply.github.com>
2023-02-08 15:57:43 +0800

    Update get_SGATarget.sql

--- app/sql/get_SGATarget.sql
+++ app/sql/get_SGATarget.sql
@@ -1,13 +1,8 @@
 SELECT (CASE
-            WHEN SGA_SIZE/POWER(2,10) < 1024 THEN
-            TO_CHAR(SGA_SIZE/POWER(2,10),'fm999999') || ' KB'
-            when
-            SGA_SIZE/POWER(2,20) < 1024 THEN
-            TO_CHAR(SGA_SIZE/POWER(2,20),'fm999999') || ' MB'
-            when SGA_SIZE/POWER(2,30) < 1024 THEN
-            TO_CHAR(SGA_SIZE/POWER(2,30),'fm999999') || ' GB'
+            when SGA_SIZE/POWER(2,10) < 1024 THEN
+            TO_CHAR(SGA_SIZE/POWER(2,10),'fm999999') || ' GB'
             else
-            TO_CHAR(SGA_SIZE/POWER(2,40),'fm999999') || ' TB' 
+            TO_CHAR(SGA_SIZE/POWER(2,20),'fm999999') || ' TB' 
             END) AS "SGA Size",
         SGA_SIZE_FACTOR AS "Size Factor",
         ESTD_DB_TIME_FACTOR AS "Estd. DB Time Factor",
