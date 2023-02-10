d17a758ab2b57ec598722e5ad14a0812c91109bb
Geno <57450665+Kkura02@users.noreply.github.com>
2023-02-09 17:49:55 -0800

    Update the SgaTarget.js

--- public/js/sgatarget.js
+++ public/js/sgatarget.js
@@ -1,7 +1,3 @@
-function tset(){
-    const targetSga = document.querySelector("#row").innerHTML
-    console.log(targetSga)
-}
 // FORMULA : ((current-new)/current)*100
 function getpercentage(targetsgarow,targetphyread){
     const curphyRead = parseInt(document.querySelector("#preads1").innerHTML)
@@ -10,25 +6,20 @@ function getpercentage(targetsgarow,targetphyread){
 
     const percentage = ((curphyRead-targetPhyRead)/curphyRead)*100
 
-    // console.log(percentage.toFixed(3))
-
-
     //rows
     //resetting the border styles of all TR
-    document.querySelectorAll("tr").forEach(el => el.style.border = "0px solid black");
+    document.querySelectorAll("tr").forEach(el => el.classList.remove("bg-gray-100","bg-gray-700"));
     const curSga = document.querySelector("#sga_row1")
 
     const targetSga = document.getElementById(targetsgarow.toString())
     
     //applying focus border on the involved rows for calculations
-    curSga.style.border = "2px solid black";
-    targetSga.style.border = "2px solid black";
+    curSga.classList.add("bg-gray-400");
+    targetSga.classList.add("bg-gray-700");
 
     let gaindisp = document.querySelector("#gaindisplay")
     let gaindispcont = document.querySelector("#gaindisplay-cont")
 
     gaindisp.innerHTML = percentage.toFixed(2)+"%"; 
     gaindispcont.classList.remove("hidden");
-    
-
 }
