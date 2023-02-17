a9fa6e736fc88a3770753bd39cd49f222b34777c
Geno <57450665+Kkura02@users.noreply.github.com>
2023-02-16 21:00:05 -0800

    removing to exchange new

--- public/js/body-loader.js
+++
@@ -1,7 +0,0 @@
-$(window).on('load',function(){
-    $(".loader").fadeOut(1000);
-    $(".content").fadeIn(1000);
-    setTimeout(function(){
-        document.getElementById("loaderz").classList.add("hidden");
-    },1000)
-})
--- public/js/spinner.js
+++
@@ -1,8 +0,0 @@
-function loadit(){
-    let usern = document.getElementById("username").value
-    let passw = document.getElementById("password").value
-  
-    if (usern != "" && passw != ""){ 
-        document.getElementById("loadIcon").classList.remove("hidden")
-    }
-  }
