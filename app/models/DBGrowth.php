ce244c141e87c08863884917deb578a0ee3526db
nrfjr <90370309+nrfjr@users.noreply.github.com>
2023-10-10 17:57:22 +0800

    Added database growth model class

---
+++ app/models/DBGrowth.php
@@ -0,0 +1,31 @@
+<?php
+
+class DBGrowth{
+
+    private $db, $fm;
+
+    public function __construct(){
+        $this->fm = new FileManager;
+    }
+
+    public function getGrowth($year)
+    {
+        $this->db = new OracleDatabase(SIDS['DEFAULT']);
+        $query = $this->fm->loadSQL('get_DBGrowth');
+
+        $param = [
+                    'p_year' => $year
+        ];
+
+        $this->db->queryWithParam($query, $param);
+
+        $result = $this->db->resultSet();
+
+        if ($result) {
+            return $result;
+        }
+		
+        return false;
+    }
+
+}
